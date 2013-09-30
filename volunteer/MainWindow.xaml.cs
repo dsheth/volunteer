4f6176a8979f4035d9527ba54cf1b2858f688107
David Sheth <david@calavista.com>
2013-09-30 08:30:24 -0500

    Buttons on the home page

--- volunteer/MainWindow.xaml.cs
+++ volunteer/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace volunteer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : NavigationWindow
     {
         public MainWindow()
         {
